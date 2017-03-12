73232745e1387301b83fb8e229565cd96eb33e11
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2017-03-12 22:52:46 +0100

    fixed

--- cpu_compute.cpp
+++ cpu_compute.cpp
@@ -42,15 +42,21 @@ double cpu_computation::calculate_intensity(
 		std::vector<point_charge_t>& charges,
 		unsigned int x,
 		unsigned int y) {
-	double total_intensity = 0.0;
+	double intensity_x = 0.0, intensity_y = 0.0;
 	for (auto charge : charges) {
 		double x_scaled = m_x_min + x * m_x_scale / (double)m_width;
 		double y_scaled = m_y_min + y * m_y_scale / (double)m_height;
-		double r_squared = (x_scaled - charge.x) * (x_scaled - charge.x) + (y_scaled - charge.y) * (y_scaled - charge.y);
-		double intensity = k * fabs(charge.charge) / r_squared;
-		if (intensity < MAX_VAL) total_intensity += intensity;
+		double dx = charge.x - x_scaled;
+		double dy = charge.y - y_scaled;
+		double r_squared = dx * dx + dy * dy;
+		double intensity = k * charge.charge / r_squared;
+		if (intensity < MAX_VAL) {
+			double r = sqrt(r_squared);
+			intensity_x += intensity * dx / r;
+			intensity_x += intensity * dy / r;
+		}
 	}
-	return total_intensity;
+	return sqrt(intensity_x * intensity_x + intensity_y * intensity_y);
 }
 
 std::vector<uint32_t> cpu_computation::to_color(std::vector<double>& intensities, Uint32 pixel_format) {
